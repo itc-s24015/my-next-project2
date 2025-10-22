b0754147415e48fc57ab6a4a30f38311ba7d3abe
s24015 <s24015@std.it-college.ac.jp>
2025-10-22 10:46:32 +0900

    chapet05

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./index.module.css";
 import Category from "../Category";
@@ -17,8 +18,7 @@ export default function NewsList({ news }: Props) {
     <ul>
       {news.map((article) => (
         <li key={article.id} className={styles.list}>
-          {/* TODO:別の章で、リンクと正式な画像に差し替えます */}
-          <div className={styles.link}>
+          <Link href={`/news/${article.id}`} className={styles.link}>
             <Image
               className={styles.image}
               src="/no-image.png"
@@ -30,10 +30,10 @@ export default function NewsList({ news }: Props) {
               <dt className={styles.title}>{article.title}</dt>
               <dd className={styles.meta}>
                 <Category category={article.category} />
-                <Date date={article.publishedAt ?? article.createdeat} />
+                <Date date={article.publishedAt ?? article.createdAt} />
               </dd>
             </dl>
-          </div>
+          </Link>
         </li>
       ))}
     </ul>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
-import { getNewsList } from "@/app/_libs/microcms";
+import { getNewsList, News } from "@/app/_libs/microcms";
 import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
+import { MicroCMSListResponse } from "microcms-js-sdk";
 
 export default async function Home() {
   const data = await getNewsList({
     limit: TOP_NEWS_LIMIT,
   });
+ 
   return (
     <>
       <section className={styles.top}>
